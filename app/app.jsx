81b1c6a66a4b6bf4642862f41ad2f5edf90c72f6
Tom Ta <tom@futureworkz.com>
2016-11-29 16:58:32 +0700

    Switch to scss

--- app/app.jsx
+++ app/app.jsx
@@ -13,7 +13,7 @@ var Examples = require('Examples');
 require("style!css!foundation-sites/dist/foundation.min.css");
 $(document).foundation();
 
-require("style!css!applicationStyles");
+require("style!css!sass!applicationStyles");
 
 ReactDOM.render(
   <Router history={hashHistory}>
